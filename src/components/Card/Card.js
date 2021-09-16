e4444ea76bd48cd3c6e303d929ca605691c470f2
unknown <dave8git@gmail.com>
2021-09-16 03:33:06 +0200

    add redux part 1

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,18 @@ import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
-const Card = props => (
-  <div className={styles.component}>
-    {props.title}
-  </div>
-);
-
-Card.propTypes = {
-  title: PropTypes.string.isRequired,
-};
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+  render() {
+    const {title} = this.props;
+    return (
+      <div className={styles.component}>
+        <h3>{title}</h3>
+      </div>
+    );
+  }
+}
 
 export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,9 +5,6 @@ import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-    state = {
-      cards: this.props.cards || [],
-    }
     static propTypes = {
       title: PropTypes.node.isRequired, 
       description: PropTypes.node,
@@ -17,28 +14,16 @@ class Column extends React.Component {
       cards: PropTypes.array,
       icon: PropTypes.node,
     }
-    addCard(title){
-      this.setState(state => (
-        {
-          columns: [
-            ...state.cards,
-            {
-              key: state.cards.length ? state.cards[state.columns.length-1].key+1 : 0,
-              title,
-            },
-          ],
-        }
-      ));
-    }
     render() {
+      const {title, icon, cards} = this.props;
       return (
         <section className={styles.component}>
-          <h3 className={styles.title}>{this.props.title}<span className={styles.icon}><Icon name={this.props.icon}/></span></h3>
+          <h3 className={styles.title}>{title}<span className={styles.icon}><Icon name={icon}/></span></h3>
           <div>
-            {this.state.cards.map(({key, ...cardProps}) => ( 
-              <Card key={key} {...cardProps} />
+            {cards.map(cardData => ( 
+              <Card key={cardData.id} {...cardData} />
             ))}
-          </div>
+          </div> 
         </section>
       );
     }
