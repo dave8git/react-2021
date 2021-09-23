cd311ce8e13ea378c99d9bc79dfd6db38e2a2191
unknown <dave8git@gmail.com>
2021-09-23 21:49:53 +0200

    fix search error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import styles from './Header.scss';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
 import { settings } from '../../data/dataStore';
+import { Search } from '../Search/Search.js';
 
 class Header extends React.Component {
   render(){
@@ -19,6 +20,7 @@ class Header extends React.Component {
               <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
               <NavLink exact to='/faq' activeClassName='active'>Faq</NavLink>
             </nav>
+            <Search />
           </div>
         </Container>
       </header>
