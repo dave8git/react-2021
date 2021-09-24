42a28e7a88e565ba58f46e35be51ce55dbdb4491
unknown <dave8git@gmail.com>
2021-09-24 09:06:07 +0200

    fix search error #2

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -55,7 +55,7 @@ class Search extends React.Component {
           onChange={event => this.handleChange(event)}
         />
         <div className={styles.buttons}>
-          <Button onClick={this.handleOK()}><Icon name={icon} /></Button>
+          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
         </div>
       </div>   
     );
